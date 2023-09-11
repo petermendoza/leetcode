990a3432240049ee09357e3e79230bcf71259586
petermendoza <peterjohnm2002@gmail.com>
2023-09-10 23:56:37 -0500

    #49 Group Anagrams

---
+++ 49.group-anagrams.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=49 lang=cpp
+ *
+ * [49] Group Anagrams
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        unordered_map<string, vector<string>> umap;
+        for (int i = 0; i < strs.size(); i++)
+        {
+            string tmp = strs[i];
+            sort(tmp.begin(), tmp.end());
+            umap[tmp].push_back(strs[i]);
+        }
+
+        vector<vector<string>> ans;
+        for (auto i : umap)
+        {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
+// @lc code=end
--- 853.car-fleet.cpp
+++ 853.car-fleet.cpp
@@ -16,32 +16,40 @@ class Solution
 public:
     int carFleet(int target, vector<int> &position, vector<int> &speed)
     {
-        stack<pair<int, int>> st;
+        // stack<pair<int, int>> st;
         vector<pair<int, int>> combined;
-        int count = 1;
-        int ans = 0;
+        // int count = 1;
+        int time = 0;
+        int ans = 1;
         for (int i = 0; i < position.size(); i++)
         {
             combined.push_back(make_pair(position[i], speed[i]));
         }
         sort(combined.begin(), combined.end());
-        for (auto i : combined)
-        {
-            st.push(i);
-        }
-        while (!st.empty())
-        {
-            if ((st.top().second * count) + st.top().first >= target)
-            {
-                while ((st.top().second * count) + st.top().first >= target)
-                {
-                    st.pop();
-                }
+        time = (target-combined[0].first)/combined[0].second;
+        for(int i = 0; i < combined.size();i++){
+            if(time<(target-combined[i].first)/combined[i].second){
                 ans++;
+                time=(target-combined[i].first)/combined[i].second;
             }
-            else
-                count++;
         }
+        // for (auto i : combined)
+        // {
+        //     st.push(i);
+        // }
+        // while (!st.empty())
+        // {
+        //     if ((st.top().second * count) + st.top().first >= target)
+        //     {
+        //         while ((st.top().second * count) + st.top().first >= target)
+        //         {
+        //             st.pop();
+        //         }
+        //         ans++;
+        //     }
+        //     else
+        //         count++;
+        // }
         return ans;
     }
 };
